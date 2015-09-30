087ceb5ba27e9bafd5eb88bf0bc91e6d4f8a533a
DM <session2@START-Coding-MBP-C02PP71ZG8WN.local>
2015-09-29 18:01:27 -0700

    Added switch statement.

--- dragonslayer-excercise.js
+++ dragonslayer-excercise.js
@@ -1,24 +1,42 @@
-/**
- * Created by session2 on 9/28/15.
- */
-var slaying = true;
-var youHit = Math.floor(Math.random() * 2);
-var damageThisRound = Math.floor(Math.random() * 5 + 1);
-var totalDamage = 0;
 
-while (slaying) {
-    if (youHit) {
-        console.log("You hit the dragon and did " + damageThisRound + " damage!");
-        totalDamage += damageThisRound;
 
-        if (totalDamage >= 4) {
-            console.log("Congrats! You won !");
-            slaying = false;
+
+
+    /**
+     * Created by session2 on 9/28/15.
+     */
+    var slaying = true;
+    var youHit = Math.floor(Math.random() * 100);
+    var damageThisRound = Math.floor(Math.random() * 5 + 1);
+    var totalDamage = 0;
+
+    while (slaying) {
+        if (youHit) {
+            console.log("You hit the dragon and did " + damageThisRound + " damage!");
+            totalDamage += damageThisRound;
+
+            if (totalDamage >= 1000000) {
+                console.log("Congrats! You won !");
+                slaying = false;
+            } else {
+                youHit = Math.floor(Math.random() * 2);
+            }
         } else {
-            youHit = Math.floor(Math.random() * 2);
+            console.log("You got utterly burned mate.");
+            slaying = false;
         }
-    } else {
-        console.log("You got uderly burned mate.");
-        slaying = false;
     }
-}
+    var dragonSlayer = prompt( "which way do you fly")
+    switch (dragonSlayer){
+        case 'left':
+            console.log("You're flying left");
+            break;
+        case 'right':
+            console.log("You're flying right");
+            break;
+        case 'straight':
+            console.log("You're flying straight");
+            break;
+        default:
+            console.log("You're flying");
+    }
